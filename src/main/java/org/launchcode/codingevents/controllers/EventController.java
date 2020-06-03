18960e438adc255e42a76d1bc4411f2b41e76064
Andy Koch <akandrewkoch@gmail.com>
2020-06-03 10:03:45 -0500

    worked through all the reading material, staring work on the exercise for model binding.  Also, event is significantly more robust than in the reading, with 5 extra fields

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,12 +1,10 @@
 package org.launchcode.codingevents.controllers;
 
-import org.launchcode.codingevents.Event;
+import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
@@ -17,13 +15,10 @@ import java.util.*;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<Event> eventList =
-            new ArrayList<>();
-
     @GetMapping
     public String displayAllEvents(Model model) {
         model.addAttribute("title", "All Events");
-        model.addAttribute("eventList", eventList);
+        model.addAttribute("eventList", EventData.getAll());
         return "events/index";
     }
 
@@ -34,11 +29,25 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName, String eventDate, String eventDescription, String eventAddress, String eventPicture) {
-        Event currentEvent = new Event(eventName,
-         eventDate, eventDescription,
-                eventAddress, eventPicture);
-        eventList.add(currentEvent);
+    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+        EventData.add(newEvent);
+        return "redirect:";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model){
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventForm (@RequestParam(required = false) int[] eventIds){
+        if (eventIds!=null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
         return "redirect:";
     }
 
---
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -0,0 +1,80 @@
+package org.launchcode.codingevents.models;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Event {
+    private int id;
+    private static int nextId =1;
+    private String name;
+    private String date;
+    private String description;
+    private int numberOfAttendees;
+    private String location;
+    private String picture;
+
+    public Event(String aName,
+                 String aDate, String aDescription,
+                 int aNumberOfAttendees,
+                 String aLocation, String aPicture){
+        this.name= aName;
+        this.date= aDate;
+        this.description= aDescription;
+        this.numberOfAttendees = aNumberOfAttendees;
+        this.location= aLocation;
+        this.picture=aPicture;
+        this.id=nextId;
+        nextId++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public int getNumberOfAttendees() {
+        return numberOfAttendees;
+    }
+
+    public int getId() { return id; }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "name='" + name + '\'' +
+                ", date='" + date + '\'' +
+                ", description='" + description + '\'' +
+                ", location='" + location + '\'' +
+                ", picture='" + picture + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
